8325e44df4c3bdc87a1fd705b30e214b88c049bd
Ayoola David <ayooladavid@gmail.com>
2020-05-25 00:24:01 +0100

    add scripts

---
+++ script.js
@@ -0,0 +1,24 @@
+// The keys and notes variables store the piano keys
+const keys = ['c-key', 'd-key', 'e-key', 'f-key', 'g-key', 'a-key', 'b-key', 'high-c-key', 'c-sharp-key', 'd-sharp-key', 'f-sharp-key', 'g-sharp-key', 'a-sharp-key'];
+const notes = [];
+keys.forEach(function(key){
+  notes.push(document.getElementById(key));
+})
+
+// Write named functions that change the color of the keys below
+const keyPlay = (event) => {
+  event.target.backgroundColor = "pink"
+}
+
+const keyReturn = (event) => {
+  event.target.backgroundColor = ""
+}
+
+// Write a named function with event handler properties
+const note = (note) => {
+    note.onmousedown = keyPlay
+    note.onmouseup = keyReturn
+}
+
+// Write a loop that runs the array elements through the function
+notes.forEach(note);
